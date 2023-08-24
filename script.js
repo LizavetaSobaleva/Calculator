367ff7184bb8ad57ba913415e6f94448e931a042
Lizaveta <sobaleva.lizaveta@gmail.com>
2023-08-24 10:25:13 +0200

    Some corrections

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ---- Calculator
 const input1 = document.getElementById('input1')
 const input2 = document.getElementById('input2')
 const submitBtn = document.getElementById('submit')
@@ -8,23 +9,31 @@ const divideBtn = document.getElementById('divide')
 const resultElement = document.getElementById('result')
 let action
 
-plusBtn.onclick = function () {
-    action = '+'
+function getAction(symbol){
+    return function(){
+        action = symbol
+    }
 }
 
-minusBtn.onclick = function () {
-    action = '-'
+plusBtn.onclick = getAction('+')
+
+minusBtn.onclick = getAction('-')
+
+multiplyBtn.onclick = getAction('*')
+
+divideBtn.onclick = getAction('/')
+
+function computeResult(inp1, inp2, actionSymbol){
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+    return eval(num1+actionSymbol+num2)
 }
 
-multiplyBtn.onclick = function () {
-    action = '*'
-}
-
-divideBtn.onclick = function () {
-    action = '/'
+function printResult(result){
+    resultElement.textContent = result
 }
 
 submitBtn.onclick = function () {
-    let result = eval(Number(input1.value) + action + Number(input2.value))
-    resultElement.textContent = result
+    let result = +computeResult(input1, input2, action).toFixed(6)
+    printResult(result)
 }
